17f5ba75222b77b232967a413bb2a18cfcff5f2d
Felipe Paloschi <paloschi.eca@gmail.com>
2019-11-22 17:01:03 -0300

    Remove modules br_sped_base and br_website_sale.

--- br_sped_base/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# © 2019 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import account_invoice
-from . import product
-from . import mrp_production
